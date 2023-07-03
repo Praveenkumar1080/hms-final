c3a1c6ef3467c12ef5eeaa89b20acfd94c916aaf
CJAYAKUM <CJAYAKUM@INLEN8520042930.corp.capgemini.com>
2023-07-03 14:00:04 +0530

    jk

--- src/main/java/com/cg/hms/Controller/com.java
+++
@@ -1,5 +0,0 @@
-package com.cg.hms.Controller;
-
-public class com {
-
-}
--- src/main/java/com/cg/hms/repository/PhysicianRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cg.hms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.hms.Entity.Physician;
-
-public interface PhysicianRepo extends JpaRepository<Physician,Integer>{
-
-}
